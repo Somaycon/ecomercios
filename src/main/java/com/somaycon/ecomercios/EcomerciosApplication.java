546fa50003abe62e3af34ae3a7e2e9c8d690be4c
Somaycon <contato.somaycon@gmail.com>
2026-09-10 13:56:47 -0300

    feat: Commit Inicial do Projeto
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/somaycon/ecomercios/EcomerciosApplication.java
@@ -0,0 +1,13 @@
+package com.somaycon.ecomercios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somaycon/ecomercios/EcomerciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somaycon.ecomercios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
